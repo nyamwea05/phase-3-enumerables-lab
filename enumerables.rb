7981ded7934fd8d7ac40602e51977aaf6101ec29
sandra <nyamweasandra@gmail.com>
2023-05-23 12:28:00 +0300

    sandra

--- enumerables.rb
+++ enumerables.rb
@@ -11,40 +11,6 @@ RSpec.describe 'Enumerable Methods' do
 
 
 
-  def get_names(spicy_foods)
-    spicy_foods.map { |food| food[:name] }
-  end
-  
-  def spiciest_foods(spicy_foods)
-    spicy_foods.select { |food| food[:heat_level] > 5 }
-  end
-  
-  def print_spicy_foods(spicy_foods)
-    spicy_foods.each do |food|
-      heat_level_emoji = '🌶' * food[:heat_level]
-      puts "#{food[:name]} (#{food[:cuisine]}) | Heat Level: #{heat_level_emoji}"
-    end
-  end
-  
-  def get_spicy_food_by_cuisine(spicy_foods, cuisine)
-    spicy_foods.find { |food| food[:cuisine] == cuisine }
-  end
-  
-  def sort_by_heat(spicy_foods)
-    spicy_foods.sort_by { |food| food[:heat_level] }
-  end
-  
-  def print_spiciest_foods(spicy_foods)
-    spiciest_foods = spiciest_foods(spicy_foods)
-    print_spicy_foods(spiciest_foods)
-  end
-  
-  def average_heat_level(spicy_foods)
-    total_heat_level = spicy_foods.sum { |food| food[:heat_level] }
-    total_heat_level / spicy_foods.length
-  end
-
-
 
 
 
